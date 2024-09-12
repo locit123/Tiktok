ba2e5a4360ba8a70c191b660d766800e2783748a
phung loc <phungloc6102003@gmail.com>
2024-09-12 13:58:07 +0700

    Dựng khung Layout mặc định

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ const publicRouters = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
     { path: '/profile', component: Profile },
+
     { path: '/upload', component: Upload, layout: HeaderOnly },
     { path: '/search', component: Search, layout: null },
 ];
