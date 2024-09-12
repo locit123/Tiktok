2d39a7d1faa820a84127d3a5626bc63b318950c0
phung loc <phungloc6102003@gmail.com>
2024-09-12 12:51:37 +0700

    Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRouters } from '~/router';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 const App = () => {
-    return <div></div>;
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    {publicRouters.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
